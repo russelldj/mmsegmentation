8b2f384c595e927c9555dbaff9f462a43eaff3eb
David Russell <davidrussell327@gmail.com>
2022-03-09 12:38:06 -0500

    Added semfire labels with different training fractions

---
+++ configs/segformer/segformer_mit-b5_512x512_10k_rui_with_sete_60_finetune.py
@@ -0,0 +1,8 @@
+_base_ = ["./segformer_mit-b0_512x512_10k_sete_60_finetune.py"]
+
+# model settings
+model = dict(
+    pretrained="/home/frc-ag-1/dev/mmsegmentation/work_dirs/segformer_mit-b5_512x512_160k_rui_with_semfire_sete_labels/iter_112000.pth",
+    backbone=dict(embed_dims=64, num_heads=[1, 2, 5, 8], num_layers=[3, 6, 40, 3]),
+    decode_head=dict(in_channels=[64, 128, 320, 512]),
+)
